a22b7591afd7dc7036f39e37fa425bb2f01be60d
Jason Steffener--Home <steffener@gmail.com>
2012-02-16 20:53:22 -0500

    Fixed the percentile code

--- Code/MatLab/RunPercentilePredict.m
+++ Code/MatLab/RunPercentilePredict.m
@@ -1,5 +1,8 @@
+BaseDir = 'C:\Users\Makaye\Desktop\Investment\ForeCast\Data';
+cd(BaseDir)
+load AMEX_487Stocks_400Days02_16_2012_2010
 
-[NTime NStock] = size(sClose);
+[NTime NStock] = size(StockClose);
 LookBackRange = 40;
 NLB = length(LookBackRange);
 TransactionFee = 10;
@@ -10,9 +13,25 @@ Value = zeros(NStock,NLB);
 
 for i = 1:NStock
     for j = 1:NLB
-        Stock = sClose(:,i);
+        Stock = flipud((StockClose(:,i)));
         LookBack = LookBackRange(j);
         [Value(i,j)] = subfnPercentile(Stock,LookBack,Money,TransactionFee);
+        %[Value, TransactionCosts, SellVector, BuyVector] = subfnPercentile((Stock),LookBack,Money,TransactionFee);
     end
 end
-
+%%
+figure(2)
+plot(Value)
+%% Figure
+index = 3;
+Name = StockList(index,:);
+Stock = flipud(StockClose(:,index));
+[V, TransactionCosts, SellVector, BuyVector] = subfnPercentile((Stock),40,Money,TransactionFee);
+V
+figure(1)
+clf
+hold on
+plot(Stock)
+plot(BuyVector,'o')
+plot(SellVector,'xr')
+title(Name)
--- Code/MatLab/subfnPercentile.m
+++ Code/MatLab/subfnPercentile.m
@@ -1,29 +1,36 @@
-function [Value, TransactionCosts] = subfnPercentile(Stock,LookBack,Money,TransactionFee)
+function [Value, TransactionCosts, SellVector, BuyVector] = subfnPercentile(Stock,LookBack,Money,TransactionFee)
 NTime = length(find(Stock>0));
 
 BuyFlag = 1;
 SellFlag = 0;
 Value = Money;
 TransactionCosts = 0;
-for j = 1:NTime - (LookBack + 1)
+
+BuyVector = zeros(NTime,1);
+SellVector = zeros(NTime,1);
+% Assume that index value 1 is the OLDEST time point
+for j = LookBack : NTime
+%for j = 1:NTime - (LookBack + 1)
     
-    currentData = Stock(j:j+LookBack-1);
+    currentData = Stock(j-LookBack+1:j);
     lowPTILE = prctile(currentData,5);
     highPTILE = prctile(currentData,95);
     
     if (Stock(j) > highPTILE) & SellFlag
         % Sell
+        SellVector(j) = Stock(j);
         BuyFlag = 1;
         SellFlag = 0;
         Value = TotalShares*Stock(j) - TransactionCosts;
         TransactionCosts = TransactionCosts + TransactionFee;
-        
+        fprintf(1,'Sell: %0.2f at %0.2f\n',Value,Stock(j));
     elseif (Stock(j) < lowPTILE) & BuyFlag
         % Buy
+        BuyVector(j) = Stock(j);
         BuyFlag = 0;
         SellFlag = 1;
         TotalShares = (Value - TransactionFee)/Stock(j);
         TransactionCosts = TransactionCosts + TransactionFee;
+        fprintf(1,'Buy: %0.4f at %0.2f\n',TotalShares, Stock(j));
     end
-end
 end
